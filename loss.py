d87e36e2595494cf0fe5a3df9b22da0d5fec53ee
shirsenduhalder <haldershirsendu1@gmail.com>
2019-04-22 17:07:29 +0530

    added loss

---
+++ loss.py
@@ -0,0 +1,19 @@
+LAMBDA = 100
+
+def discriminator_loss(disc_real_output, disc_generated_output):
+
+	real_loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=tf.ones_like(disc_real_output), logits=disc_real_output)
+
+	generated_loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=tf.zeros_like(disc_generated_output),logits=disc_generated_output)
+
+	total_disc_loss = real_loss + generated_loss
+
+	return total_disc_loss
+
+def generator_loss(disc_generated_output, generated_output, target):
+
+	gan_loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=tf.ones_like(disc_generated_output), logits=disc_generated_output)
+	l1_loss = tf.reduce_mean(tf.abs(target - generated_output))
+	total_gan_loss = tf.add(gan_loss, tf.multiply(LAMBDA, l1_loss))
+
+	return total_gan_loss
